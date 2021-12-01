ab4480387b17db9ed533bf8f8af0c3d58955af3e
Mohamed Lafi <molafi3000@hotmail.com>
2021-12-01 17:15:13 +0000

    commit

--- voltage.c
+++ voltage.c
@@ -18,13 +18,14 @@ void init_adc(void)
 {
 	// | (1 << ADATE) 
 	// | (1 << ADLAR) high bits reading
-	ADMUX = (1 << REFS0); // setting the multiplexer register
-	ADCSRA = (1 << ADEN) | (1 << ADSC) | (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1);
+	ADMUX |= (1 << REFS0); // setting the multiplexer register
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADPS2) | (1 << ADPS1);
 }
 
 uint16_t read_adc(void)
 {
 	// You might need to set the ADLAR to 1 to read the high bits.
+	// ADMUX |= (1 < ADLAR);
 	return ADC;
 }
 
@@ -34,10 +35,9 @@ int main(void)
 	uint16_t result;
 	double voltage;
 	init_debug_uart0();
-	init_adc();
-	while(1) 
+	while(1)
 	{
-		init_adc();
+		init_adc(); // This will restart the conversion back.
 		result = read_adc();
 		voltage = (result * 3.3) / 1024;
 		printf("%4d : %6.5fV\n", result, voltage);
